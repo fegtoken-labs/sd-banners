6d796aaee7dd9c29d4754255de0d91c5688ec03f
FEGfilip <fegfilip@gmail.com>
2026-06-19 18:10:07 +0200

    tbsc init

--- redirect-mixed-case.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var url = window.location.pathname;
-    var lowerCaseUrl = url.toLowerCase();
-    if (url !== lowerCaseUrl) {
-        window.location.replace(lowerCaseUrl);
-    }
-});
